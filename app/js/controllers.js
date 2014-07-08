eb302707e088329017b7aa458ee0447372ade208
Peter Malmgren <ptmalmgren@gmail.com>
2014-07-07 22:14:30 -0400

    Added unit tests for controllers

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,17 +5,12 @@
 (function() {
 	var app = angular.module('myApp.controllers', []);
 	
-	app.controller('PostsController', function($scope, blogAPI) {
+	app.controller('PostsController', ['$scope', 'blogAPI', function($scope, blogAPI) {
 		$scope.posts = [];
 		
-		blogAPI.getPosts().success(function (result) {
-			$scope.numPages = Math.floor(result.length / 5);
+		blogAPI.getPosts().then(function (result) {
 			$scope.posts = result;
-
-			for (var i = result.length - 1; i >= 0; i--) {
-				
-			};
 		});
-	});
+	}]);
 
 })();
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -2,14 +2,47 @@
 
 /* jasmine specs for controllers go here */
 
-describe('controllers', function(){
-  beforeEach(module('myApp.controllers'));
+describe('Blog post tests', function() {
+  var posts = [{"title": "Test title 1","body": "Test body 1","date": "Test date 1"}, {"title": "Test title 2","body": "Test body 2","date": "Test date 2"}];
 
+  beforeEach(module('myApp'));
 
-  it('should initialize', inject(function($controller) {
-    //spec body
-    var myCtrl1 = $controller('PostsController', { $scope: {} });
-    expect(myCtrl1).toBeDefined();
+  var scope, fakeAPI, controller, q, deferred;
+
+  beforeEach(function() {
+    fakeAPI = {
+      getPosts: function() {
+        deferred = q.defer();
+        deferred.resolve(posts);
+        return deferred.promise;
+      }
+    };
+    spyOn(fakeAPI, 'getPosts').andCallThrough();
+  });
+
+  beforeEach(inject(function ($rootScope, $controller, $q) {
+    q = $q;
+  	scope = $rootScope.$new();
+  	controller = $controller('PostsController', { $scope: scope, blogAPI: fakeAPI });
   }));
+
+  it('posts should not be defined', function() {
+    expect(scope.posts).not.toBe([]);
+  });
+
+  it('should be defined after the scope is initialized', function() {
+    scope.$apply();
+    expect(scope.posts).toBeDefined();
+  });
+
+  it('should call the factory method after initalization', function() {
+    scope.$apply();
+    expect(fakeAPI.getPosts).toHaveBeenCalled();
+  });
+
+  it('should have 2 posts', function() {
+    scope.$apply();
+    expect(scope.posts.length).toBe(2);
+  });
 
 });
